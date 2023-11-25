04e5970c9cd969aa1bdc1caead31f39713333fb9
Esthera Dow <estheradow@MacBook-Pro-Esthera.local>
2023-11-25 12:39:35 +0100

    Add conversion functions to hand class

--- src/domain/Hands.py
+++ src/domain/Hands.py
@@ -1,3 +1,4 @@
+import itertools
 from dataclasses import dataclass
 from enum import Enum
 
@@ -8,16 +9,47 @@ class Chirality(Enum):
 
 
 @dataclass
-class Landmark:
+class Knuckle:
     x: float
     y: float
     z: float
 
 
+# https://developers.google.com/mediapipe/solutions/vision/hand_landmarker
 @dataclass
 class Hand:
-    landmarks: list[Landmark]
+    knuckles: list[Knuckle]
     handedness: Chirality
+
+    def get_index(self) -> Knuckle:
+        return self.knuckles[8]
+
+    def get_base(self) -> Knuckle:
+        return self.knuckles[0]
+
+    def convert_to_relative_coordinates(self, relative_knuckle_list=None) -> list:
+        base_x, base_y = 0, 0
+        for index, knuckle in enumerate(self.knuckles):
+            if index == 0:
+                base_x, base_y = knuckle.x, knuckle.y
+
+            relative_knuckle_list[index][0] = knuckle.x - base_x
+            relative_knuckle_list[index][1] = knuckle.y - base_y
+        return relative_knuckle_list
+
+    def flatten_list(self, knuckle_list) -> list:
+        return list(itertools.chain.from_iterable(knuckle_list))
+
+    def normalize(self, knuckle_list) -> list:
+        max_value = max(list(map(abs, knuckle_list)))
+
+        def normalize_(n):
+            return n / max_value
+
+        return list(map(normalize_, knuckle_list))
+
+    def prepare_for_model(self) -> list:
+        return self.normalize(self.flatten_list(self.convert_to_relative_coordinates()))
 
 
 @dataclass
